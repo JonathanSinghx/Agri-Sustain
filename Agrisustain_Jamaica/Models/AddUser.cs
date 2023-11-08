b03aa7d350de64f5d09c0de2c932232133310e4b
Nicolette Woolery <nicolette.woolery@mcsystems.com>
2023-11-08 13:58:24 -0500

    real trial

---
+++ Agrisustain_Jamaica/Models/AddUser.cs
@@ -0,0 +1,32 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.ComponentModel.DataAnnotations;
+
+namespace Agrisustain_Jamaica.Models
+{
+    public class AddUserModel
+    {
+        [Required]
+        public string FirstName { get; set; }
+
+        [Required]
+        public string LastName { get; set; }
+
+        [Required]
+        public string Email { get; set; }
+
+        [Required]
+        public string Password { get; set; }
+
+        [Required]
+        public string PhoneNumber { get; set; }
+
+        [Required]
+        public string UserType { get; set; }
+
+        [Required]
+        public string Location { get; set; }
+
+        public List<Badge> EarnedBadges { get; set; }
+    }
+
+}
---
+++ Agrisustain_Jamaica/Models/Badge.cs
@@ -0,0 +1,23 @@
+﻿namespace Agrisustain_Jamaica.Models
+{
+    public class Badge
+    {
+        public string Name { get; set; }
+        public string Description { get; set; }
+        public string Icon { get; set; }
+
+    }
+
+    public class BadgeCriteria
+    {
+        public string Name { get; set; }
+        public int RequiredCount { get; set; }
+    }
+
+    public class BadgeProgress
+    {
+        public string BadgeName { get; set; }
+        public int DaysUsed { get; set; }
+        public string ProgressMessage { get; set; }
+    }
+}
