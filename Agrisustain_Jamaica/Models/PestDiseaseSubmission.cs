6157cfb8887931549f8f62c4ce63c75a4d2c6c2c
Nicolette Woolery <nicolette.woolery@mcsystems.com>
2023-11-20 14:07:40 -0500

    sprint3end

---
+++ Agrisustain_Jamaica/Models/PestDiseaseSubmission.cs
@@ -0,0 +1,13 @@
+﻿namespace Agrisustain_Jamaica.Models
+{
+    public class PestDiseaseSubmission
+    {
+        public int Id { get; set; }
+        public string FarmLocation { get; set; }
+        public DateTime DateOfSighting { get; set; }
+        public string Pest { get; set; }
+        public string Disease { get; set; }
+        public string Description { get; set; }
+        public string PhotoPath { get; set; }
+    }
+}
